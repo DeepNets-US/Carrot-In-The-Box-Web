fdf4dbec8a290cd3477ed49fdb24e929a99c0c89
Utkarsh Saxena <deepnets722@gmail.com>
2023-12-05 07:28:37 +0530

    Playing Functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const player1Image = document.getElementById("player1Box");
     const player2Image = document.getElementById("player2Box");
 
+    // Get the message Output
+    const msg = document.getElementById("msg");
+
     // Get the Name Inputs
     const player1Name = document.getElementById("player1Name");
     const player2Name = document.getElementById("player2Name");
 
     // Image Sources
+    const closedBox = "../Images/Boxes/close.png"
     const carrotBox = '../Images/Boxes/carrot.png';
     const emptyBox = '../Images/Boxes/empty.png';
+
+    // Game Variables
+    let carrotFound = false;
+    let player1Clicked = false;
+    let player2Clicked = false;
 
     // Function to change the image source
     function changeImageSource(imageElement, newSource) {
@@ -18,51 +27,80 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Function to simulate 50/50 probability
-    function getRandomCarrot() {
-        return Math.random() < 0.5;
+    function getRandomCarrot(player) {
+        if (!carrotFound) {
+            if (Math.random() < 0.5) {
+                player.src = carrotBox;
+                carrotFound = true;
+                msg.innerHTML = `
+                Wohh!! You got the carrot!
+                <br>
+                <p class="fs-5">Will you tell him?</p>
+                `
+            } else {
+                player.src = emptyBox;
+                carrotFound = false;
+                msg.innerHTML = `
+                Oops!! You don't got the carrot!
+                <br>
+                <p class="fs-5">Will you fool him?</p>
+                `
+            }
+        } else {
+            player.src = closedBox;
+            carrotFound = false;
+            msg.innerHTML = `
+            Box closed! ${player1Name.value}, keep your eyes closed!
+                <br>
+                <p class="fs-5">Try another box!</p>
+                
+            `
+        }
     }
 
     // Function to add the 'activename' class
-    function addNameActive(nameElement) {
-        nameElement.classList.add('activename');
+    function addNameActive(addElem, removeElem) {
+        addElem.classList.add('activename');
+        removeElem.classList.remove('activename');
     }
 
-    // Function to remove the 'activename' class
-    function removeNameActive(nameElement) {
-        nameElement.classList.remove('activename');
-    }
-
-    // Event listener function for player 1 box click
     function player1ClickHandler() {
-        const playerGetsCarrot = getRandomCarrot();
-        if (playerGetsCarrot) {
-            changeImageSource(player1Image, carrotBox);
-            addNameActive(player1Name);
-            removeNameActive(player2Name);
+        if (!player1Clicked) {
+            addNameActive(player1Name, player2Name);
+            player2Image.removeEventListener('click', player2ClickHandler);
+            getRandomCarrot(player1Image);
+            player1Clicked = true;
         } else {
-            changeImageSource(player1Image, emptyBox);
-            addNameActive(player2Name);
-            removeNameActive(player1Name);
+            changeImageSource(player1Image, closedBox);
+            carrotFound = false;
+            msg.innerHTML = `
+            Box closed! ${player1Name.value}, keep your eyes closed!
+            <br>
+            <p class="fs-5">Try another box!</p>
+                
+            `
+            player1Clicked = false;
         }
-        player1Image.removeEventListener('click', player1ClickHandler);
     }
 
-    // Event listener function for player 2 box click
     function player2ClickHandler() {
-        const playerGetsCarrot = getRandomCarrot();
-        if (playerGetsCarrot) {
-            changeImageSource(player2Image, carrotBox);
-            addNameActive(player2Name);
-            removeNameActive(player1Name);
+        if (!player2Clicked) {
+            addNameActive(player2Name, player1Name);
+            player1Image.removeEventListener('click', player1ClickHandler);
+            getRandomCarrot(player2Image);
+            player2Clicked = true;
         } else {
-            changeImageSource(player2Image, emptyBox);
-            addNameActive(player1Name);
-            removeNameActive(player2Name);
+            changeImageSource(player2Image, closedBox);
+            carrotFound = false;
+            msg.innerHTML = `
+            Box closed! ${player1Name.value}, keep your eyes closed!
+            <br>
+            <p class="fs-5">Try another box!</p>    
+            `
+            player2Clicked = false;
         }
-        player2Image.removeEventListener('click', player2ClickHandler);
     }
 
-    // Adding click event listeners to player 1 and player 2 boxes
     player1Image.addEventListener('click', player1ClickHandler);
     player2Image.addEventListener('click', player2ClickHandler);
 
